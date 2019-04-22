ee32a9f59ee496a146135af444daeb41b03e7da4
tehfedaykin <jennifer.wadella@gmail.com>
2019-04-22 11:52:49 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'galaxy-rating-app';
+}
